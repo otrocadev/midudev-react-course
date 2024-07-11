7850aee8693b6d572cda0622cd1de34582e5f5e1
otrocadev <otrocadev@gmail.com>
2024-07-11 11:35:13 +0200

    Adding 3rd project

--- projects/02-mouse-follower/src/App.jsx
+++ projects/02-mouse-follower/src/App.jsx
@@ -1,20 +1,48 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [enabled, setEnabled] = useState(false);
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x: 0, y: 0})
 
   useEffect(() => {
-    console.log("efecto");
-  });
+    // getEventListeners(object) on CONSOLE to check the current events
+
+    const handleMove = (event) => {
+      const {clientX, clientY} = event
+      setPosition({x: clientX, y: clientY})
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => { // executes every time that the useEffect refreshes or on unMount
+      window.removeEventListener('pointermove', handleMove)
+    } 
+  }, [enabled]) // DEPENDENCES - if empty it executes every time the component renderizes
 
   return (
-    <>
-      <h3>Patata</h3>
+    <main>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+      ></div>
       <button onClick={() => setEnabled(!enabled)}>
-        {enabled ? "Desactivar" : "Activar"} seguir puntero
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
-    </>
-  );
+    </main>
+  )
 }
 
-export default App;
+export default App
